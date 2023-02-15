e9c7de92ce9f5a71781c79b7f780ce060d1b699f
Marek Zelinka <mzelinka17@gmail.com>
2023-02-15 18:32:42 +0100

    make peroson input name required

--- src/components/person-form.jsx
+++ src/components/person-form.jsx
@@ -30,6 +30,7 @@ export function PersonForm(props) {
             type="text"
             name="name"
             value={formData.name}
+            required
             onChange={handleChange}
           />
         </label>
