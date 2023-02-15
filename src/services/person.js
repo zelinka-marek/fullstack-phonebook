fe1f610bc9dc28f9c9ddb941d19908665f355369
Marek Zelinka <mzelinka17@gmail.com>
2023-02-15 18:30:46 +0100

    refactor service

--- src/services/person.js
+++ src/services/person.js
@@ -2,21 +2,35 @@ const baseUrl = "http://localhost:3001/persons";
 const headers = new Headers();
 headers.set("Content-Type", "application/json");
 
+function toJSON(response) {
+  return response.then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
+  });
+}
+
 export function getPersons() {
-  return fetch(baseUrl).then((response) => response.json());
+  return toJSON(fetch(baseUrl));
 }
 
 export function createPerson(data) {
-  return fetch(baseUrl, {
-    method: "post",
-    headers: headers,
-    body: JSON.stringify(data),
-  }).then((response) => response.json());
+  return toJSON(
+    fetch(baseUrl, {
+      method: "post",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+  );
 }
 
 export function deletePerson(id) {
-  return fetch(`${baseUrl}/${id}`, {
-    method: "delete",
-    headers: headers,
-  }).then((response) => response.json());
+  return toJSON(
+    fetch(`${baseUrl}/${id}`, {
+      method: "delete",
+      headers: headers,
+    })
+  );
 }
