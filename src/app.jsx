d8157ac34ff3ed16f1ef8cea8501c35c83b3641a
Marek Zelinka <mzelinka17@gmail.com>
2023-02-15 13:32:40 +0100

    fetch persons

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NameFilter } from "./components/name-filter";
 import { PersonForm } from "./components/person-form";
 import { PersonList } from "./components/person-list";
 
-export function App(props) {
-  const { persons: initialPersons } = props;
-  const [persons, setPersons] = useState(initialPersons);
+export function App() {
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
   const personsToShow =
     filter === ""
       ? persons
       : persons.filter((person) => new RegExp(filter, "i").test(person.name));
+
+  useEffect(() => {
+    fetch("http://localhost:3001/persons")
+      .then((response) => response.json())
+      .then(setPersons);
+  }, []);
 
   const addPerson = (newPerson) => {
     const nameExists = persons.find((person) => person.name === newPerson.name);
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,8 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./styles/index.css";
 
-const persons = [
-  { id: 1, name: "Arto Hellas", tel: "202-555-0150" },
-  { id: 2, name: "Ada Lovelace", tel: "202-555-0160" },
-  { id: 3, name: "Ryan Florence", tel: "202-555-0196" },
-  { id: 4, name: "Dan Abramov", tel: "12-43-234345" },
-  { id: 5, name: "Mary Poppendieck", tel: "39-23-6423122" },
-];
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App persons={persons} />
+    <App />
   </StrictMode>
 );
